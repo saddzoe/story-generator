166594df21b2d65804f0e512de260000f6b0a8f5
Mohammed Olaiya <mohammedolaiya@gmail.com>
2021-03-17 22:38:12 -0400

    Update README.py

--- README.py
+++ README.py
@@ -8,6 +8,6 @@ name = ['Darth Vader', 'Undertaker', 'John Cena', 'Ash Ketchum', 'Lelouch', 'Lig
 who = ['a dinosaur', 'a pig', 'a judge', 'a lawywer', 'a king', 'her boyfrined', 'his girlfriend', 'the queen', 'the prince']
 residence = ['Hogwarts', 'Ghana', 'Nigeria', 'Brazil', 'South Africa', 'London', 'Canada', 'Pluto', 'Saturn', 'Houston', 'India']
 went = ['to school', 'wedding', 'moon', 'space', 'movies', 'hospital', 'college', 'vet', 'army', 'play']
-happened = ['saw shooting stars', 'saw The Weeknd', 'went shopping', 'saw Michael Jackson', 'listened to House of Ballons', 'fought the Big Show', 'found King Booker', 'worked out to Ariana Grande', 'went on a spaceship to see bettleguese', 'saw Anubus', 'meet Cleopatra', 'worked out with Barney', 'fought along side the X men', 'saw th Avengers and Thanos battle for the world', 'fought along side Harry, Hermoine, and Ron', 'teamed up with Voldermont']
+happened = ['saw shooting stars', 'saw The Weeknd', 'went shopping', 'saw Michael Jackson', 'listened to House of Ballons', 'fought the Big Show', 'found King Booker', 'worked out to Ariana Grande', 'went on a spaceship to see bettleguese', 'saw Anubus', 'meet Cleopatra', 'worked out with Barney', 'fought along side the X men', 'saw the Avengers and Thanos battle for the world', 'fought along side Harry, Hermoine, and Ron', 'teamed up with Voldermont']
 
 print(random.choice(when) + ', ' + random.choice(who) + ' that visited ' + random.choice(residence) + ', went to the ' + random.choice(went) + ' and ' + random.choice(happened))
